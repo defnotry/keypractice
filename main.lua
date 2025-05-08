3b676bf171b7c0c33e328582772ae9086e642e66
Ryan Christian Rayo <ryan.rayo@outlook.com>
2025-05-08 15:19:41 +0800

    dsa

--- main.lua
+++ main.lua
@@ -1,8 +1,11 @@
-local key = game:GetService("ScriptContext").RequestUrl:match("key=([^&]+)")
-
-if key and key == "test" then
-    print("Key is valid")
-else
-    print("Key is invalid")
+-- main.lua
+-- return a function you can call with the key
+return function(key)
+    local VALID_KEY = "test"
+    if key == VALID_KEY then
+        print("Key is valid")
+        -- ... do the rest of your logic here
+    else
+        warn("Key is invalid")
+    end
 end
-
